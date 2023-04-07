fc4998b636cfd5590c1ca9d9350fa89406a772fb
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-04-07 23:02:13 +0300

    input addition, reading from file and tokenizing, deleting whitespaces, comments done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+        int row, col = 1;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line = line.trim();
+            line = line.split("~")[0];
+            int parenthesesCount = 0;
+            String[] tokensWithComments= line.split(" ");
+            for (int i = 0; i < tokensWithComments.length ; i++) {
+                    System.out.println(tokensWithComments[i]);
+            }
+
+        }
     }
 }
